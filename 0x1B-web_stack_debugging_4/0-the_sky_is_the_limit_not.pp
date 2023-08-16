b15b8bee5b6e9f85a364d400992c6eb9e5c84d02
BEChukwunwike <blessingechukwunwike@gmail.com>
2023-08-16 09:26:25 -0700

    debugging

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,11 @@
-# fix our stack so that we get to 0 errors
-exec { 'file limit':
-  onlyif   => 'test -e /etc/default/nginx',
-  command  => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
-  provider => shell,
+# manuscript that increases the amount of traffic an NGINX server can handle
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path => '/usr/local/bin/:/bin/'
+} ->
+
+#Restart NGINX server
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path => '/etc/init.d/'
 }
